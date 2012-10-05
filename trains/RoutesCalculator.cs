491772e127a1f3492bff05656b56d383347f5e3d
Acaz <acazsouza@gmail.com>
2012-10-04 23:14:37 -0300

    failed attempt to implement an algorithm to solve the problem. i'll try to implement dijkstra algorithm next

--- trains/RoutesCalculator.cs
+++ trains/RoutesCalculator.cs
@@ -45,31 +45,93 @@ namespace trains
             return distance;
         }
 
-        public int? NumberOfPossibleTrips(City startCity, City endCity, int numberOfStops)
+        /*private IEnumerable<Route> GetPossibleRoutesEndIn(City city)
         {
-            int possibleTrips = 0;
-
-            for (int i = 0; i < (cities.Count() - 1); i++)
-            {
-                Route route;
-
-                try
-                {
-                    route = Routes.Where(x => x.StartCity.Id == cities[i].Id && x.EndCity.Id == cities[i + 1].Id).First();
-                }
-                catch (Exception ex)
-                {
-                    return -1;
-                }
-
-                distance += route.Distance;
-            }
-
-            return possibleTrips;
+            return Routes.Where(x =>  x.EndCity.Id == city.Id);
         }
 
-        /*public int NumberOfTrips(char )
+        private IEnumerable<Route> GetPossibleRoutesStartIn(City city)
         {
+            return Routes.Where(x => x.StartCity.Id == city.Id);
         }*/
+
+        public int? NumberOfPossibleTrips(City startCity, City endCity, int numberOfStops)
+        {
+            IList<IEnumerable<Route>> possibleTrips = new List<IEnumerable<Route>>();
+            IList<Route> trip = new List<Route>();
+
+            foreach (Route route in Routes.Where(x => x.EndCity.Id == endCity.Id))
+            {
+                trip.Add(route);
+
+                if (route.StartCity.Id == startCity.Id)
+                {
+                    trip = new List<Route>();
+                    possibleTrips.Add(trip);
+                } else
+                {
+                    foreach (Route route2 in Routes.Where(x => x.EndCity.Id == route.StartCity.Id))
+                    {
+                        trip.Add(route2);
+
+                        if (route2.StartCity.Id == startCity.Id)
+                        {
+                            trip = new List<Route>();
+                            possibleTrips.Add(trip);
+                        }
+                        else
+                        {
+                            foreach (Route route3 in Routes.Where(x => x.EndCity.Id == route2.StartCity.Id))
+                            {
+                                trip.Add(route3);
+
+                                if (route3.StartCity.Id == startCity.Id)
+                                {
+                                    trip = new List<Route>();
+                                    possibleTrips.Add(trip);
+                                }
+                                else
+                                {
+                                    foreach (Route route4 in Routes.Where(x => x.EndCity.Id == route3.StartCity.Id))
+                                    {
+                                        trip.Add(route4);
+
+                                        if (route4.StartCity.Id == startCity.Id)
+                                        {
+                                            trip = new List<Route>();
+                                            possibleTrips.Add(trip);
+                                        }
+                                        else
+                                        {
+                                            foreach (Route route5 in Routes.Where(x => x.EndCity.Id == route4.StartCity.Id))
+                                            {
+                                                trip.Add(route5);
+
+                                                if (route5.StartCity.Id == startCity.Id)
+                                                {
+                                                    trip = new List<Route>();
+                                                    possibleTrips.Add(trip);
+                                                }
+                                                else
+                                                {
+
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return possibleTrips.Count();
+        }
+
+        public string Teste(string teste)
+        {
+            return Teste(teste);
+        }
     }
 }
